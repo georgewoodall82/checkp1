99c98f0de14686f2a21848d8b287be71cbdb9709
georgewoodall82 <47471634+georgewoodall82@users.noreply.github.com>
2020-02-10 07:48:41 +0000

    Bug Fixes

--- checkp1.sh
+++ checkp1.sh
@@ -1,3 +1,4 @@
+sudo rm -f checkra1n
 sudo wget https://assets.checkra.in/downloads/linux/cli/arm/5f7d4358971eb2823413801babbac0158524da80c103746e163605d602ff07bf/checkra1n
 sudo chmod +x checkra1n
 sudo ./checkra1n
